7937ebcd18a47a83ab81efbaf749c944ae392505
tetiana <tetyanaomeliuta@gmail.com>
2023-08-20 08:55:56 +0300

    remarks

--- src/App.js
+++ src/App.js
@@ -116,45 +116,6 @@ function App() {
       }
     }
 
-    // if (name === 'male'){
-
-    //   if (state.male === true){
-    //     if (state.result) {
-    //       filteredContacts = state.result.filter((result) => {
-    //          return result.gender === name })
-    //     } else {
-    //       filteredContacts = state.contacts.filter((contacts) => {
-    //         return contacts.gender === name })
-    //     }
-    //     console.log(name)
-    //    } else {
-    //     console.log(false)
-    //    }
-
-    // }
-
-    // }
-    // console.log(arg, arg1, arg2, arg3)
-    //     if (arg1 === true ){
-    //       alert(111)
-    //       arg1 = 'male';
-    //       filteredContacts = state.result.filter((result) => {
-    //         return (result.gender === arg1  )})
-    //       }
-
-    //     if (arg2 === true ){
-    //         arg2 = 'female';
-    //         filteredContacts = state.result.filter((result) => {
-    //           return (result.gender === arg2  )})
-    //         }
-
-    //     if (arg3 === true ){
-    //           arg3='';
-    //           filteredContacts = state.result.filter((result) => {
-    //             return (result.firstName.toLowerCase().includes(arg.toLowerCase()) || result.lastName.toLowerCase().includes(arg.toLowerCase())
-    //           || result.phone.toLowerCase().includes(arg.toLowerCase()) ) && (result.gender === arg3  )})
-    //           }
-
     if (!state.result) {
       filteredContacts = state.contacts.filter((contacts) => {
         return contacts.firstName.toLowerCase().includes(arg.toLowerCase());
@@ -168,33 +129,7 @@ function App() {
         {" "}
         Введіть дані для пошуку:
         <input id="search" name="search" onChange={handleSearchChange} />
-        {/* <div>
-          <input
-            type="checkbox"
-            name="male"
-            value={isCheckedMale}
-            onChange={handleSearchChange}
-          />
-          <label for="male">чоловіча</label>
-        </div>
-        <div>
-          <input
-            type="checkbox"
-            name="female"
-            value={isCheckedFemale}
-            onChange={handleSearchChange}
-          />
-          <label for="female">жіноча</label>
-        </div>
-        <div>
-          <input
-            type="checkbox"
-            name="nospecify"
-            value={isCheckedNospecify}
-            onChange={handleSearchChange}
-          />
-          <label for="nospecify">не вказано</label>
-        </div> */}
+       
       </div>
       <div className="App">
         {!state.result
